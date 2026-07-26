9d383ab626b502f776f927b232ad5bbbee8d680f
Prabal Gade <prabalgade1@gmail.com>
2026-07-26 18:41:42 +0530

    Added the GCD Code

---
+++ Basic_Math/GCDorHCF.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class GCDorHCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        GCDorHCF obj = new GCDorHCF();
+        int gcd = obj.GCD(num1, num2);
+        System.out.println("The GCD (HCF) of " + num1 + " and " + num2 + " is: " + gcd);
+    
+    }
+    public int GCD(int n1, int n2) 
+    {
+        while(n1>0 && n2>0)
+        {
+            if(n1>n2)
+            {
+                n1=n1%n2;
+            }
+            else
+            {
+                n2=n2%n1;
+            }
+        }
+        if(n1==0)
+        {
+            return n2;
+        }
+        else
+        {
+            return n1;
+        }
+    }
+}
--- Basic_Math/PrimeNumber.java
+++ Basic_Math/PrimeNumber.java
@@ -1,6 +1,6 @@
 /*A prime number is a number which has only 2 factors */
 import java.util.*;
-public class PrimeNumber {
+public class PrimeNumber {1
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
