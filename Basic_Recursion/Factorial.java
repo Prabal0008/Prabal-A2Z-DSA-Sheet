b1d8af14f0d985605b9a7414e58fcc1134936e27
Prabal Gade <prabalgade1@gmail.com>
2026-07-28 20:06:17 +0530

    Added Factorial program

---
+++ Basic_Recursion/Factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args)
+     {
+         System.out.println("Main begin ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        System.out.println("Sum is : "+factorial(n));
+        System.out.println("Main end ");  
+        
+    }
+    
+
+     public static int factorial(int n)
+    {
+        if(n==0)
+        {
+           
+            return 1;
+        }
+        else
+        { 
+            return n*factorial(n-1);
+        }
+       
+    }
+}
